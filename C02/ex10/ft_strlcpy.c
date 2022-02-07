856da4d43a8e3644a8705973a22d707c91972414
Alexis R <64582375+alexisradice@users.noreply.github.com>
2022-02-07 20:40:08 +0100

    Add files via upload

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aradice <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/06 16:58:06 by aradice           #+#    #+#             */
+/*   Updated: 2022/02/06 22:04:59 by aradice          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	y;
+
+	i = 0;
+	y = 0;
+	while (src[y])
+		y++;
+	if (size > 0)
+	{
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	dest[i] = '\0';
+	return (y);
+}
